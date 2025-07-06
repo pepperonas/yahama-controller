8bec9c33d4c6c84b86b9b6192d8ea24a77da0e50
Martin <martin@example.com>
2025-07-06 02:43:00 +0200

    changed port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5001;
 
 // Enable CORS for all routes
 app.use(cors());
